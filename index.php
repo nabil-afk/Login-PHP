78b1295a188a642674430d74c2b2f90bca9e595b
nabildzakwan <nabildzakwanbernadine@gmail.com>
2020-11-12 16:41:04 +0700

    login php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Utama</title>
+</head>
+<body>
+   <h1>Halaman Utama</h1>
+   <hr>
+   <a href="login.php"><button>Login</button></a>
+   <a href="admin.php"><button>Admin</button></a>
+   <a href="guru.php"><button>Guru</button></a>
+   <a href="siswa.php"><button>Siswa</button></a>    
+</body>
+</html>
